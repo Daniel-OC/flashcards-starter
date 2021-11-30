8cf7d72f5f453cb78de91860ec4f05e6fe4785c8
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-11-29 20:29:24 -0800

    Add 'it' portions of the remaining tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -19,8 +19,20 @@ describe('Turn', function() {
 	it('should accept an argument for the user\'s guess and the current card object', function() {
 		const turn = new Turn(guess, card);
 		expect(guess).to.be.arguments
-		expect(guess).to.be.arguments
+		expect(card).to.be.arguments
 	})
 
+	it('should only accept guess as a string')
+	it('should only accept an object for the card')
+	it('should have a returnGuess method')
+	it('should return the guess')
+	it('should return the card object')
+	it('should have an evaluateGuess method')
+	it('should require a guess to run')
+	it('should return true if the user\'s guess was correct')
+	it('should return false if the user\'s guess was incorrect')
+	it('should have a giveFeedback method')
+	it('should return "correct!" if the user\'s guess was correct')
+	it('should return "incorrect" if the user\'s guess was incorrect')
 	
 })
