8524b64645fcd439b836c5fd0fe95724b7493bfa
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-11-30 22:36:27 -0800

    Add to round-test, going to bed for the night

--- test/Round-test.js
+++ test/Round-test.js
@@ -10,16 +10,35 @@ describe('Round', function() {
 
 	beforeEach(() => {
 		const cards = data.prototypeData.map((info) => new Card(info.id, info.question, info.answers, info.correctAnswer))
-		const deck = new Deck(cards)
-		const round = new Round(deck)
+		let deck = new Deck(cards)
+		let turn = new Turn(null, deck.shift())
+		const round = new Round(deck, turn)
+		
 	})
 
-	it('should have a returnCurrentCard function')
-	it('should return the card currently being played')
-	it('should have a takeTurn method')
-	it('should create a new instance of the Turn class')
-	it('should update the turns count')
-	it('should return the current card to the end of the deck, and update the current card')
+	it('should have a returnCurrentCard function', function() {
+		expect(round.returnCurrentCard).to.be.a('function')
+	})
+	it('should return the card currently being played', function() {
+		expect(round.returnCurrentCard()).to.equal(deck[0])
+	})
+	it('should have a takeTurn method', function() {
+		expect(round.takeTurn).to.be.a('function')
+	})
+	it('should create a new instance of the Turn class', function() {
+		expect(round.takeTurn()).to.be.an('object')
+	})
+	it('should update the turns count', function() {
+		let turnCount1 = round.turnCount
+		round.takeTurn()
+		let turnCount2 = round.turnCount
+		expect(turnCount2 - turnCount1).to.equal(1)
+	})
+	it('should return the current card to the end of the deck, and update the current card', function() {
+		round.takeTurn()
+		expect(round.deck[0].id).to.equal(2)
+		expect(round.deck[29].id).to.equal(1)
+	})
 	it('should record an incorrect guess in the array incorrectGuesses')
 	it('should tell the user if they got the answer wrong')
 	it('should tell the user if they got the answer right')
