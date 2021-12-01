9267fa5b3c796dad4aa00b5de1ed9ef7c3551431
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-11-30 22:07:39 -0800

    Write out initial skeleton of round-test

---
+++ test/Round-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai')
+const turn = require('../src/Turn.js')
+const Deck = require('../src/Deck.js')
+const expect = chai.expect
+
+describe('Round', function() {
+
+it('should have a returnCurrentCard function')
+it('should return the card currently being played')
+it('should have a takeTurn method')
+it('should create a new instance of the Turn class')
+it('should update the turns count')
+it('should return the current card to the end of the deck, and update the current card')
+it('should record an incorrect guess in the array incorrectGuesses')
+it('should tell the user if they got the answer wrong')
+it('should tell the user if they got the answer right')
+it('should calculate what percentage of the guesses the user got correct')
+it('should tell the user when the round is over, as well as their score')
+
+
+})
+
+
